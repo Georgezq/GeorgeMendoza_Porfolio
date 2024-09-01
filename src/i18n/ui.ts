89712d9e42df03a695c93cc7ea4896c2e2b7c6f8
Georgezq <yosho2mendoza@gmail.com>
2024-09-01 17:00:44 -0500

    first commit

---
+++ src/i18n/ui.ts
@@ -0,0 +1,24 @@
+export const languages = {
+    es: 'Español',
+    en: 'Ingles',
+  };
+  
+  export const defaultLang = 'es';
+  
+  export const ui = {
+    es: {
+      'nav.home':'Inicio',
+      'nav.sobremi':'Sobre Mi',
+      'nav.projects':'Proyectos',
+      'nav.training':'Formacion', 
+    },
+
+    en: {
+      'nav.home':'Home',
+      'nav.sobremi':'About Me',
+      'nav.projects':'Projects',
+      'nav.training':'Training', 
+    },
+  } as const;
+  
+  export const showDefaultLang = false;
